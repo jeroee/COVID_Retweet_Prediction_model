f412caa672463ec674b3cb8b1e86412b0932e7c2
Jeremy Ng <56480078+jeroee@users.noreply.github.com>
2021-08-09 14:12:48 +0800

    update gitignore

---
+++ Dataset/raw_dataset.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+wget "https://zenodo.org/record/3871753/files/TweetsCOV19.tsv.gz"
+gzip -d TweetsCOV19.tsv.gz
+
+wget "https://zenodo.org/record/4593502/files/TweetsCOV19_052020.tsv.gz"
+gzip -d TweetsCOV19_052020.tsv.gz
+
+wget "https://zenodo.org/record/4593524/files/TweetsCOV19_062020_122020.tsv.gz"
+gzip -d TweetsCOV19_062020_122020.tsv.gz
