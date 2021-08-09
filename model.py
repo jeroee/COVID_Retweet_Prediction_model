6be60db1f2eac86207dce3e787bf6e56dce125df
wilbertaristo <45594146+wilbertaristo@users.noreply.github.com>
2021-08-09 15:24:39 +0800

    Add final model deetz

--- model.py
+++ model.py
@@ -14,17 +14,19 @@ import pytz
 import matplotlib.pyplot as plt
 
 class nn_Regression(nn.Module):
-    def __init__(self,input_features,dropout,model_name):
+    def __init__(self,input_features, dropout, model_name):
         super(nn_Regression,self).__init__()
-        self.fc1 = nn.Linear(input_features,256)
-        self.fc2 = nn.Linear(256,64)
-        self.fc3 = nn.Linear(64,16)
-        self.fc4 = nn.Linear(16,1)
-#         self.fc5 = nn.Linear(8,1)
-        self.batchnorm1 = nn.BatchNorm1d(256)
-        self.batchnorm2 = nn.BatchNorm1d(64)
-        self.batchnorm3 = nn.BatchNorm1d(16)
-#         self.batchnorm4 = nn.BatchNorm1d(8)
+        self.fc1 = nn.Linear(input_features,512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256,128)
+        self.fc4 = nn.Linear(128,64)
+        self.fc5 = nn.Linear(64,1)
+
+        self.batchnorm1 = nn.BatchNorm1d(512)
+        self.batchnorm2 = nn.BatchNorm1d(256)
+        self.batchnorm3 = nn.BatchNorm1d(128)
+        self.batchnorm4 = nn.BatchNorm1d(64)
+
         self.dropout = nn.Dropout(dropout)
         self.model_name = model_name
     
@@ -33,7 +35,6 @@ class nn_Regression(nn.Module):
         x = self.batchnorm1(x)
         x = F.relu(x)
         x = self.dropout(x)
-        
         
         x = self.fc2(x)
         x = self.batchnorm2(x)
@@ -45,12 +46,12 @@ class nn_Regression(nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
         
-#         x = self.fc4(x)
-#         x = self.batchnorm4(x)
-#         x = F.relu(x)
-#         x = self.dropout(x)
-        
         x = self.fc4(x)
+        x = self.batchnorm4(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        
+        x = self.fc5(x)
         y_pred = F.relu(x)
         return y_pred
 
